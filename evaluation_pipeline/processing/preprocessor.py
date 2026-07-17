58958a00934d71fe5ecef23d408f6f89f6efa145
Akhil T V <140319506+Akhil-0190@users.noreply.github.com>
2026-07-17 19:39:04 +0530

    Delete evaluation_pipeline directory

--- evaluation_pipeline/processing/preprocessor.py
+++
@@ -1,7 +0,0 @@
-def preprocess(df):
-    df['error_flag'] = (df['level'] == 'ERROR').astype(int)
-    df['warn_flag'] = (df['level'] == 'WARN').astype(int)
-
-    features = df[['response_time', 'error_flag', 'warn_flag']]
-
-    return features
